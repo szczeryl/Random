1b1e2e20018afd9d91f80a73eca83aa8fa2cb04c
szczeryl <qwerty844448@gmail.com>
2025-09-15 20:55:34 +0800

    Create main.m

---
+++ EE2211/main.m
@@ -0,0 +1,15 @@
+%this is the MATLAB code for EE2211 assignment 1, which I used to corroborate my solutions with.
+Y = [-3; 2; 1; 5; 4];
+(X' * X)^-1
+
+ans =
+
+    0.0246   -0.0021
+   -0.0021    0.0058
+
+>> w = X\Y
+
+w =
+
+    0.7203
+   -0.4170
