53b7f8f2bc40cb4dfa932608c2f605cbe180f341
szczeryl <qwerty844448@gmail.com>
2025-10-01 16:59:04 +0800

    Create TrafficLight_ArduinoIDE.ino

---
+++ TrafficLight_ArduinoIDE.ino
@@ -0,0 +1,106 @@
+//Feb 2025 
+// three different coloured LEDs (green, yellow, red)#
+//was bored one weekend and bought 100 LEDs for fun so I decided to simply make something fun. Finished it in less than an hour as I just copy pasted the code and changed delay for the blinking effect.
+//nothing fancy at all
+
+int green = 7; 
+int yellow = 8; 
+int red = 9;
+
+void setup(){
+  pinMode(red, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(green,  OUTPUT);
+}
+
+void loop(){
+digitalWrite(red, HIGH);
+ delay(5000);
+digitalWrite(red,  LOW);
+
+  digitalWrite(yellow, HIGH);
+delay(100);
+  digitalWrite(yellow,  LOW);
+delay(500);
+  digitalWrite(yellow, HIGH);
+delay(100);
+  digitalWrite(yellow,  LOW);
+delay(500);
+  digitalWrite(yellow, HIGH);
+delay(100);
+  digitalWrite(yellow,  LOW);
+delay(250);
+  digitalWrite(yellow, HIGH);
+delay(250);
+  digitalWrite(yellow,  LOW);
+delay(100);
+  digitalWrite(yellow, HIGH);
+delay(100);
+  digitalWrite(yellow,  LOW);
+delay(100);
+  digitalWrite(yellow, HIGH);
+delay(100);
+  digitalWrite(yellow,  LOW);
+delay(100);
+  digitalWrite(yellow, HIGH);
+delay(100);
+  digitalWrite(yellow, LOW);
+delay(100);
+  digitalWrite(yellow, HIGH);
+delay(50);
+  digitalWrite(yellow, LOW);
+delay(100);
+  digitalWrite(yellow, HIGH);
+delay(50);
+  digitalWrite(yellow, LOW);
+delay(100);
+  digitalWrite(yellow, HIGH);
+delay(50);
+  digitalWrite(yellow, LOW);
+delay(100);
+
+digitalWrite(green, HIGH);
+delay(5000);
+digitalWrite(green,  LOW);
+
+
+digitalWrite(yellow, HIGH);
+delay(100);
+  digitalWrite(yellow,  LOW);
+delay(500);
+  digitalWrite(yellow, HIGH);
+delay(100);
+  digitalWrite(yellow,  LOW);
+delay(500);
+  digitalWrite(yellow, HIGH);
+delay(100);  
+  digitalWrite(yellow, LOW);
+delay(250);
+  digitalWrite(yellow, HIGH);
+delay(100);
+  digitalWrite(yellow, LOW);
+delay(250);
+  digitalWrite(yellow, HIGH);
+delay(100);
+  digitalWrite(yellow, LOW);
+delay(100);
+  digitalWrite(yellow, HIGH);
+delay(100);
+  digitalWrite(yellow,  LOW);
+delay(100);
+  digitalWrite(yellow, HIGH);
+delay(100);
+  digitalWrite(yellow,  LOW);
+delay(100);
+  digitalWrite(yellow, HIGH);
+delay(50);
+  digitalWrite(yellow,  LOW);
+delay(100);
+  digitalWrite(yellow, HIGH);
+delay(50);
+  digitalWrite(yellow,  LOW);
+delay(100);
+  digitalWrite(yellow, HIGH);
+delay(50);
+  digitalWrite(yellow,  LOW);
+delay(100); 
